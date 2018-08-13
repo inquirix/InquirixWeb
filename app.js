b0c048e3b0cb300cea3eb80ea91c548ec03ae95b
XMachinus <jsbparson@gmail.com>
2018-08-13 08:09:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -26,10 +26,11 @@ const server = app.listen(3000, (err) => {
 })
 
 
-const con = mysql.createConnection({
-    host: "10.67.71.7",
-    user: "root",
-    password: "98835Piggy98835!"
+const connection = mysql.createConnection({
+    host     : '10.67.71.7',
+    user     : 'root',
+    password : '98835Piggy98835!',
+    database : 'students'
   });
 
 const io = socket(server);
