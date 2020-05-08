e715c2348ddffee8e61ae5c77ef85fba7c6dcd8a
Kwaku <boafo819@gmail.com>
2020-05-08 12:13:18 -0400

    Homepage fixes

--- start.js
+++ start.js
@@ -39,62 +39,6 @@ const server = app.listen(process.env.PORT || 3000, (err) => {
 
 const io = socket(server);
 
-
-
-// const io = socket(server);
-
-// var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
-// app.use(sess({
-//    name: 'JSESSION',
-//    secret: 'MYSECRETISVERYSECRET',
-//    store:  store,
-//    resave: true,
-//    saveUninitialized: true
-// }));
-
-// app.use(flash());
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.use('local', new LocalStrategy({
-
-//     usernameField: 'username',
-
-//     passwordField: 'password',
-
-//     passReqToCallback: true //passback entire req to call back
-// }, function(req, username, password, done) {
-//     if (!username || !password) {
-//         return done(null, false, req.flash('message', 'All fields are required.'));
-//     }
-//     var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
-//     connection.query("select * from tbl_users where username = ?", [username], function(err, rows) {
-//         console.log(err);
-//         console.log(rows);
-//         if (err) return done(req.flash('message', err));
-//         if (!rows.length) {
-//             return done(null, false, req.flash('message', 'Invalid username or password.'));
-//         }
-//         salt = salt + '' + password;
-//.body         var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
-//         var dbPassword = rows[0].password;
-//         if (!(dbPassword == encPassword)) {
-//             return done(null, false, req.flash('message', 'Invalid username or password.'));
-//         }
-//         return done(null, rows[0]);
-//     });
-// }));
-
-// passport.serializeUser(function(user, done) {
-//     done(null, user.id);
-// });
-
-// passport.deserializeUser(function(id, done) {
-//     connection.query("select * from tbl_users where id = " + id, function(err, rows) {
-//         done(err, rows[0]);
-//     });
-// });
-
 app.get('/homepage', function(req, res) {
     res.render('HTML/index', {});
 });
@@ -276,3 +220,57 @@ io.on('connection', (socket) => {
         io.sockets.emit(data.chat_id, data);
     })
 })
+
+// const io = socket(server);
+
+// var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
+// app.use(sess({
+//    name: 'JSESSION',
+//    secret: 'MYSECRETISVERYSECRET',
+//    store:  store,
+//    resave: true,
+//    saveUninitialized: true
+// }));
+
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// passport.use('local', new LocalStrategy({
+
+//     usernameField: 'username',
+
+//     passwordField: 'password',
+
+//     passReqToCallback: true //passback entire req to call back
+// }, function(req, username, password, done) {
+//     if (!username || !password) {
+//         return done(null, false, req.flash('message', 'All fields are required.'));
+//     }
+//     var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
+//     connection.query("select * from tbl_users where username = ?", [username], function(err, rows) {
+//         console.log(err);
+//         console.log(rows);
+//         if (err) return done(req.flash('message', err));
+//         if (!rows.length) {
+//             return done(null, false, req.flash('message', 'Invalid username or password.'));
+//         }
+//         salt = salt + '' + password;
+//.body         var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
+//         var dbPassword = rows[0].password;
+//         if (!(dbPassword == encPassword)) {
+//             return done(null, false, req.flash('message', 'Invalid username or password.'));
+//         }
+//         return done(null, rows[0]);
+//     });
+// }));
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user.id);
+// });
+
+// passport.deserializeUser(function(id, done) {
+//     connection.query("select * from tbl_users where id = " + id, function(err, rows) {
+//         done(err, rows[0]);
+//     });
+// });
